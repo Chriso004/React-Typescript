573db423086e53babfd39e260c38155c66974f52
Chriso004 <qudcksrlf1@gmail.com>
2022-02-21 15:13:58 +0900

    Props test

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+interface AppProps {
+  header: string;
+  extra?: string;
+}
 
-function App() {
+const App = ( { header, extra= "default" }: AppProps ) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>{header}</h1>
+      {extra && <p>{extra}</p>}
+    </>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,7 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App header="this is header" />
   </React.StrictMode>,
   document.getElementById('root')
 );
