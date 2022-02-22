f063976c9fd8b7a634631dbdc03b418c56fcd984
Chriso004 <qudcksrlf1@gmail.com>
2022-02-22 10:12:10 +0900

    state practice

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
-interface AppProps {
-  header: string;
-  extra?: string;
-}
+import { useState } from 'react';
 
-const App = ( { header, extra= "default" }: AppProps ) => {
+const App = () => {
+  const [count, setCount] = useState<number>(0);
+  const title = <h1 id='title'>this is title</h1>
+  const IncreaseCount = () => setCount(count + 1);
+  const DecreaseCount = () => setCount(count - 1);
   return (
     <>
-      <h1>{header}</h1>
-      {extra && <p>{extra}</p>}
+    { title }
+    < h3 > total clicks: { count }</h3>
+      <button id='increaseButton' onMouseDown={IncreaseCount}>Increase </button>
+      <button id='decreaseButton' onMouseDown={DecreaseCount}>Decrease</button>
     </>
-  );
+  )
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import StatePractice from "./StatePractice";
+import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <StatePractice />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
