59d5782314733e9e52bc376bbed3b7c658e89764
Chriso004 <qudcksrlf1@gmail.com>
2022-02-22 10:59:30 +0900

    event arg add

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,25 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 
 const App = () => {
   const [count, setCount] = useState<number>(0);
+  console.log(count);
   const title = <h1 id='title'>this is title</h1>
-  const IncreaseCount = () => setCount(count + 1);
-  const DecreaseCount = () => setCount(count - 1);
+
+  const IncreaseCount = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    e.preventDefault();
+    setCount(count + 1);
+  }
+
+  const DecreaseCount = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    e.preventDefault();
+    setCount(count - 1);
+  }
+  
   return (
     <>
-    { title }
-    < h3 > total clicks: { count }</h3>
-      <button id='increaseButton' onMouseDown={IncreaseCount}>Increase </button>
+      {title}
+      < h3 > total clicks: {count}</h3>
+      <button id='increaseButton' onMouseDown={IncreaseCount}>Increase</button>
       <button id='decreaseButton' onMouseDown={DecreaseCount}>Decrease</button>
     </>
   )
